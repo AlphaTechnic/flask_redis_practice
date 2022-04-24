0f5cb69a71e30923218c394fd3838840edab63e4
AlphaTechnic <juho3471@gmail.com>
2022-04-24 11:33:15 +0900

    [refactor] csrf 관련 조치, user_views.bp 블루프린트 등록

--- my_forum/__init__.py
+++ my_forum/__init__.py
@@ -1,16 +1,19 @@
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import CSRFProtect
 
 import config
 
 db = SQLAlchemy()
 migrate = Migrate()
+csrf = CSRFProtect()
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(config)
+    csrf.init_app(app)
 
     # ORM
     db.init_app(app)
@@ -18,7 +21,11 @@ def create_app():
     from . import models
 
     # 블루프린트
-    from .views import main_views
+    from .views import main_views, user_views, board_views, board_article_views, dashboard_views
     app.register_blueprint(main_views.bp)
+    app.register_blueprint(user_views.bp)
+    # app.register_blueprint(board_views.bp)
+    # app.register_blueprint(board_article_views.bp)
+    # app.register_blueprint(dashboard_views.bp)
 
     return app
