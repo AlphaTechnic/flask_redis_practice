630162c864fceb272321f1c76c884513006b1cb0
AlphaTechnic <juho3471@gmail.com>
2022-04-25 00:01:35 +0900

    [feat] 대시보드 view 작업 완료

---
+++ my_forum/views/dashboard_views.py
@@ -0,0 +1,40 @@
+from flask import Blueprint, g
+from flask_api import status
+
+g_dashboard_size = 10
+
+bp = Blueprint('dashboard', __name__, url_prefix='/')
+
+
+@bp.route('/dashboard/', methods=['GET'])
+def get_dashboard():
+    def make_resp_body():
+        body = dict()
+        body["message"] = f"게시판마다 최대 {g_dashboard_size} 개의 post 조회 성공"
+        body["data"] = dict()
+        boards = g.user.boards.all()
+        body["data"]["dashboard_num"] = len(boards)
+        body["data"]["dashboard_list"] = make_dashboard(boards)
+        return body
+
+    def make_dashboard(boards):
+        arr_of_dashboard = []
+        boards.sort(key=lambda board: board.create_date, reverse=True)
+        for board in boards:
+            dashboard = dict()
+            dashboard["board_id"] = board.id
+            dashboard["board_name"] = board.name
+            posts = sorted(board.posts.all(), key=lambda post: post.create_date, reverse=True)[:g_dashboard_size]
+            dashboard["posts"] = [{"post_id": post.id, "post_title": post.title} for post in posts]
+            arr_of_dashboard.append(dashboard)
+        return arr_of_dashboard
+
+    body = make_resp_body()
+    return body, status.HTTP_200_OK
+
+
+
+
+
+
+
