cfa96a2e6e7a2f935a3598cedd236af7c6824c8c
AlphaTechnic <juho3471@gmail.com>
2022-04-24 11:34:11 +0900

    [feat] UserCreateForm 생성

--- my_forum/forms.py
+++ my_forum/forms.py
@@ -1,15 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
 class UserCreateForm(FlaskForm):
     username = StringField("아이디",
-                           validators=[DataRequired(), Length(min=4, max=20)])
+                           validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField("이메일",
                         validators=[DataRequired(), Email()])
     password = PasswordField("비밀번호",
                              validators=[DataRequired(), Length(min=4, max=20)])
     confirm_password = PasswordField("비밀번호 확인",
                                      validators=[DataRequired(), EqualTo("password")])
-    submit = SubmitField("가입")
+
+    # username = StringField("아이디")
+    # email = StringField("이메일")
+    # password = PasswordField("비밀번호")
+    # confirm_password = PasswordField("비밀번호확인")
