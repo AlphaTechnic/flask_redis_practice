7aad46abf2a2e2eb31b2c5f121bcb296a5dd3232
AlphaTechnic <juho3471@gmail.com>
2022-04-24 09:22:26 +0900

    [refactor] table 관계 재정립

--- my_forum/models.py
+++ my_forum/models.py
@@ -7,19 +7,35 @@ class User(db.Model):
     password = db.Column(db.String(200), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
 
+    # 다른 테이블과의 관계
+    posts = db.relationship('Post', backref='user', lazy='dynamic')
+
+    def __repr__(self):
+        return f"user{self.id}/{self.username[:10]}"
+
 
 class Board(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
-    user = db.relationship('User', backref=db.backref('board_set', cascade='all, delete-orphan'))
     name = db.Column(db.String(200), nullable=False)
     create_date = db.Column(db.DateTime(), nullable=False)
+
+    # 다른 테이블과의 관계
+    posts = db.relationship('Post', backref='board', lazy='dynamic')
+
+    def __repr__(self):
+        return f"board{self.id}/{self.name[:10]}"
 
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    board_id = db.Column(db.Integer, db.ForeignKey('board.id', ondelete='CASCADE'))
-    board = db.relationship('Board', backref=db.backref('post_set', cascade='all, delete-orphan'))
+
     title = db.Column(db.String(200), nullable=False)
     content = db.Column(db.Text(), nullable=False)
     create_date = db.Column(db.DateTime(), nullable=False)
+
+    # 다른 테이블과의 관계
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
+    board_id = db.Column(db.Integer, db.ForeignKey('board.id', ondelete='CASCADE'))
+
+    def __repr__(self):
+        return f"{self.board}->post{self.id}/{self.title[:10]}"
