3eb5a34abf80ec239277f1726883d1e536003706
AlphaTechnic <juho3471@gmail.com>
2022-04-24 13:27:37 +0900

    [refactor] 잔버그 수정(response message 조건문 오류), 함수명 변경(make_resp -> make_resp_body)

--- my_forum/views/user_views.py
+++ my_forum/views/user_views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, session
+from flask import Blueprint, request, session, make_response, jsonify
 from flask_api import FlaskAPI, status
 
 from my_forum.forms import UserCreateForm, UserLoginForm
@@ -9,16 +9,16 @@ from my_forum import db
 bp = Blueprint('user', __name__, url_prefix='/')
 
 
-@bp.route('/signup/', methods='POST')
+@bp.route('/signup/', methods=['POST'])
 def signup():
-    def make_resp(success):
-        resp = dict()
-        resp["data"] = {}
+    def make_resp_body(success):
+        body = dict()
+        body["data"] = {}
         if success:
-            resp["message"] = "가입 성공"
+            body["message"] = "가입 성공"
         else:
-            resp["message"] = "이미 가입된 메일"
-        return resp
+            body["message"] = "이미 가입된 메일"
+        return body
 
     def create_user(form):
         user = User(
@@ -28,48 +28,58 @@ def signup():
         )
         db.session.add(user)
         db.session.commit()
-
     form = UserCreateForm()
     # flash(form.errors)
     if request.method == 'POST' and form.validate():
         user = User.query.filter_by(email=form.email.data).first()
+        # user = db.session.query(User).filter_by(email=form.email.data).first()
         if not user:
             create_user(form)
-            resp = make_resp(success=True)
-            return resp, status.HTTP_200_OK
-        resp = make_resp(success=False)
-        return resp, status.HTTP_409_CONFLICT
+            body = make_resp_body(success=True)
+            return body, status.HTTP_200_OK
+        body = make_resp_body(success=False)
+        return body, status.HTTP_409_CONFLICT
 
     return {"message": "유효하지 않은 요청"}, status.HTTP_400_BAD_REQUEST
 
 
-@bp.route('/login/', methods='POST')
+@bp.route('/login/', methods=['POST'])
 def login():
-    def make_resp(user_in_db, valid_password=True):
-        resp = dict()
-        resp["data"] = {}
-        if user_in_db and valid_password:
-            resp["message"] = "로그인 성공"
-        elif user_in_db and not valid_password:
-            resp["message"] = "유효하지 않은 password"
+    def make_resp_body(user_in_db, valid_password=True):
+        body = dict()
+        body["data"] = {}
+        if user_in_db:
+            if valid_password:
+                body["message"] = "로그인 성공"
+            else:
+                body["message"] = "유효하지 않은 password"
+            return body
 
-        resp["message"] = "존재하지 않는 사용자"
-        return resp
+        body["message"] = "존재하지 않는 사용자"
+        return body
+
+    def save_in_server_session(user):
+        session.clear()
+        session['user_id'] = user.email
+
+    def response_with_cookie(user):
+        body = make_resp_body(user_in_db=True, valid_password=True)
+        response = make_response(jsonify(body), status.HTTP_200_OK)
+        response.set_cookie("user_id", value=user.email)
+        return response
 
     form = UserLoginForm()
     if request.method == 'POST' and form.validate():
-        user = User.query.filter_by(username=form.email.data).first()
+        user = User.query.filter_by(email=form.email.data).first()
         if not user:
-            resp = make_resp(user_in_db=False)
-            return resp, status.HTTP_404_NOT_FOUND
+            body = make_resp_body(user_in_db=False)
+            return body, status.HTTP_404_NOT_FOUND
         elif not check_password_hash(user.password, form.password.data):
-            resp = make_resp(user_in_db=True, valid_password=False)
-            return resp, status.HTTP_400_BAD_REQUEST
+            body = make_resp_body(user_in_db=True, valid_password=False)
+            return body, status.HTTP_400_BAD_REQUEST
 
-        session.clear()
-        session['user_id'] = user.id
-        resp = make_resp(user_in_db=True, valid_password=True)
-        return resp, status.HTTP_200_OK
+        save_in_server_session(user)
+        response = response_with_cookie(user)
+        return response
 
     return {"message": "유효하지 않은 요청"}, status.HTTP_400_BAD_REQUEST
-
